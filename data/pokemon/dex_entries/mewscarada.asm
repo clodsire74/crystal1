e2da2eb0fbd10fc51454c4b2e7c02d1e13b765f3
clodsire74 <davidstormking0@gmail.com>
2025-02-23 10:35:43 -0800

    Update mewscarada.asm

--- data/pokemon/dex_entries/mewscarada.asm
+++ data/pokemon/dex_entries/mewscarada.asm
@@ -1,1 +1,10 @@
-
++	db "MAGICIAN@" ; species name
++	dw 411, 688 ; height, weight
++
++	db   "With misdirection,"
++	next "it rigs foes with"
++	next "flower bombs."
++
++	page "It sets them off"
++	next "before its opponents"
++	next "realize.@"
